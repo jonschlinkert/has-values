97f2ab99ccbcde36f03d135d67acbaad240b8dcc
jonschlinkert <jon.schlinkert@sellside.com>
2016-03-27 08:11:34 -0400

    lint tests, remove should

--- test.js
+++ test.js
@@ -7,92 +7,92 @@
 
 'use strict';
 
-var should = require('should');
+require('mocha');
+var assert = require('assert');
 var hasValues = require('./');
 
 function isEmpty(o, isZero) {
   return !hasValues(o, isZero);
 }
 
-describe('hasValues', function () {
-  it('should work for nulls', function () {
-    hasValues(null).should.be.false;
-    hasValues(undefined).should.be.false;
+describe('hasValues', function() {
+  it('should work for nulls', function() {
+    assert(!hasValues(null));
+    assert(!hasValues(undefined));
   });
 
-  it('should work for booleans', function () {
-    hasValues(true).should.be.true;
-    hasValues(false).should.be.true;
+  it('should work for booleans', function() {
+    assert(hasValues(true));
+    assert(hasValues(false));
   });
 
-  it('shold work for strings', function () {
-    hasValues('').should.be.false;
-    hasValues('string').should.be.true;
+  it('shold work for strings', function() {
+    assert(!hasValues(''));
+    assert(hasValues('string'));
   });
 
-  it('should work for numbers', function () {
-    hasValues(0).should.be.true;
-    hasValues(1).should.be.true;
+  it('should work for numbers', function() {
+    assert(hasValues(0));
+    assert(hasValues(1));
   });
 
-  it('should treat zero as null when `noZero` is set', function () {
-    hasValues(0, true).should.be.false;
+  it('should treat zero as null when `noZero` is set', function() {
+    assert(!hasValues(0, true));
   });
 
-  it('should work for objects', function () {
-    hasValues({}).should.be.false;
-    hasValues({a: 'b'}).should.be.true;
+  it('should work for objects', function() {
+    assert(!hasValues({}));
+    assert(hasValues({a: 'b'}));
   });
 
-  it('should work for arrays', function () {
-    hasValues([]).should.be.false;
-    hasValues(['a', 'b']).should.be.true;
+  it('should work for arrays', function() {
+    assert(!hasValues([]));
+    assert(hasValues(['a', 'b']));
   });
 
-  it('should work for functions', function () {
-    hasValues(function (foo) {}).should.be.true;
-    hasValues(function () {}).should.be.false;
+  it('should work for functions', function() {
+    assert(hasValues(function(foo) {}));
+    assert(!hasValues(function() {}));
   });
 });
 
-
-describe('isEmpty', function () {
-  it('should work for nulls', function () {
-    isEmpty(null).should.be.true;
-    isEmpty(undefined).should.be.true;
+describe('isEmpty', function() {
+  it('should work for nulls', function() {
+    assert(isEmpty(null));
+    assert(isEmpty(undefined));
   });
 
-  it('should work for booleans', function () {
-    isEmpty(false).should.be.false;
-    isEmpty(true).should.be.false;
+  it('should work for booleans', function() {
+    assert(!isEmpty(false));
+    assert(!isEmpty(true));
   });
 
-  it('shold work for strings', function () {
-    isEmpty('').should.be.true;
-    isEmpty('string').should.be.false;
+  it('shold work for strings', function() {
+    assert(isEmpty(''));
+    assert(!isEmpty('string'));
   });
 
-  it('should work for numbers', function () {
-    isEmpty(0).should.be.false;
-    isEmpty(1).should.be.false;
+  it('should work for numbers', function() {
+    assert(!isEmpty(0));
+    assert(!isEmpty(1));
   });
 
-  it('should treat zero as null when `noZero` is set', function () {
-    isEmpty(0, true).should.be.true;
+  it('should treat zero as null when `noZero` is set', function() {
+    assert(isEmpty(0, true));
   });
 
-  it('should work for objects', function () {
-    isEmpty({}).should.be.true;
-    isEmpty({a: 'b'}).should.be.false;
+  it('should work for objects', function() {
+    assert(isEmpty({}));
+    assert(!isEmpty({a: 'b'}));
   });
 
-  it('should work for arrays', function () {
-    isEmpty([]).should.be.true;
-    isEmpty(['a', 'b']).should.be.false;
+  it('should work for arrays', function() {
+    assert(isEmpty([]));
+    assert(!isEmpty(['a', 'b']));
   });
 
-  it('should work for functions', function () {
-    isEmpty(function (foo) {}).should.be.false;
-    isEmpty(function () {}).should.be.true;
+  it('should work for functions', function() {
+    assert(!isEmpty(function(foo) {}));
+    assert(isEmpty(function() {}));
   });
 });
