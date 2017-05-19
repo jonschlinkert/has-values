b2285f3587d1a785ec1cda44873a138383cec2d1
jonschlinkert <jon.schlinkert@sellside.com>
2017-05-19 13:18:01 -0400

    run update

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 /*!
  * has-values <https://github.com/jonschlinkert/has-values>
  *
- * Copyright (c) 2014-2015, Jon Schlinkert.
- * Licensed under the MIT License.
+ * Copyright (c) 2014-2015, 2017, Jon Schlinkert.
+ * Released under the MIT License.
  */
 
 'use strict';
